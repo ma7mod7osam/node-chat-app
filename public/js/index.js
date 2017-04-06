d00c75cabfd6f1d4ba11623155287368981deced
ma7mod7osam <ma7mod7osam@gmail.com>
2017-04-06 13:05:22 +0000

    greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ var socket = io();
     socket.on('connect', function () {
         console.log('connect to server');
     });
+    
     socket.on('disconnect', function () {
        console.log('disconnected from server'); 
     });
--- server/server.js
+++ server/server.js
@@ -12,16 +12,26 @@ var io = socketIO(server);
 
 io.on('connection', (socket) => {
    console.log('new user connected');
+   socket.emit('newMessage', {
+       from: 'Admin',
+       text: 'Welcome To the chat App',
+       date: new Date().getTime()
+   });
+   socket.broadcast.emit('newMessage', {
+       from: 'Admin',
+       text: 'new User has joined',
+       date: new Date().getTime()
+   });
    
    socket.on('disconnect', () => {
    console.log('user disconnect from server'); 
    });
    
    socket.on('createMessage', (message) => {
-      io.emit('newMessage', {
-       from: message.from,
-       text: message.text,
-       date: new Date().getTime()
+    socket.broadcast.emit('newMessage', {
+        from: message.from,
+        text: message.text,
+        date: new Date().getTime()  
       });
    });
 });
