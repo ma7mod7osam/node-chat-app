e1c430ae627c4674f78cb556acbaee969759be2d
ma7mod7osam <ma7mod7osam@gmail.com>
2017-04-06 12:23:23 +0000

    add js folder and index.js

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+
+var socket = io();
+            
+    socket.on('connect', function () {
+        console.log('connect to server');
+    });
+    socket.on('disconnect', function () {
+       console.log('disconnected from server'); 
+    });
+
+     socket.emit('createMessage', {
+         text: ' hello there ',
+         from: 'mahmood'
+           });
+           
+    socket.on('newMessage', (message) => {
+      console.log(`from : ${message.from} >> ${message.text} >>> ${message.date}`); 
+   });
+ 
